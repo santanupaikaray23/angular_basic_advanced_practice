b9b79be63a6311db3aeb69a9bd16cc4ac45fdea5
LAPTOP-KLF3DVEL\santa <santanupaikaray1996@gmail.com>
2023-12-13 12:40:57 +0530

    add all files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SampleComponent } from './components/sample/sample.component';
+import { DisplayComponent } from './components/display/display.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SampleComponent,
+    DisplayComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/sample/sample.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-sample',
+  templateUrl: './sample.component.html',
+  styleUrl: './sample.component.css'
+})
+export class SampleComponent implements OnInit {
+
+  constructor(){
+
+  }
+
+  ngOnInit(){
+
+  }
+
+}
